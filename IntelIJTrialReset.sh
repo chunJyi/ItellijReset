ca3cb3cb3dee3f3d1122684bc10d7c978c886012
chunJyi <chunjyicj@gmail.com>
2022-04-15 20:35:50 +0630

    com

---
+++ IntelIJTrialReset.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm ~/.IntelliJIdea*/config/eval/idea*.evaluation.key && \
+sed -i '/evlsprt/d' ~/.IntelliJIdea*/config/options/other.xml && \
+rm -rf ~/.java/.userPrefs/jetbrains
+
+# It is Highly Advised to Purchase the JetBrain Softwares
+# This is only for the case You just want to Extend the 
+# Trial Period and Evaluate the IDE for some more Time
